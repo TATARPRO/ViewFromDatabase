050431976701fe3d27e19d844eab9ba46e549596
TATARPRO <tatarprofessional@gmail.com>
2020-09-12 04:06:17 +0100

    Creating a theme provider to serve all views from database

---
+++ RazorFromDatabase/Models/ThemeDirectoryContent.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RazorFromDatabase.Models
+{
+    public class ThemeDirectoryContent : IDirectoryContents
+    {
+        private readonly ILongRepository<Page> _pageContext;
+        private readonly IServiceProvider _serviceProvider;
+        private string _relativePath;
+        private const string directoryPattern = "/Areas/Views/InnertPages/";
+
+        public ThemeDirectoryContent(IServiceProvider serviceProvider, string relativePath)
+        {
+            _serviceProvider = serviceProvider;
+            var pageService = serviceProvider.GetRequiredService<ILongRepository<Page>>();
+            _pageContext = pageService;
+            _relativePath = relativePath;
+        }
+
+        public bool Exists
+        {
+            get
+            {
+                if (_relativePath.Contains(directoryPattern))
+                    return true;
+                return false;
+            }
+        }
+
+        public IEnumerator<IFileInfo> GetEnumerator()
+        {
+            var files = _pageContext.Return().ToList();
+            foreach (var item in files)
+            {
+                yield return new ThemeFileInfo(_serviceProvider, item.Location);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            var files = _pageContext.Return().ToList();
+            return files.GetEnumerator();
+        }
+    }
+}
---
+++ RazorFromDatabase/Services/ThemeFileProvider.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+using System;
+using RazorFromDatabase.Models;
+
+namespace RazorFromDatabase.Services
+{
+    public class ThemeFileProvider : IFileProvider
+    {
+        //private readonly ILongRepository<Page> _context;
+        private readonly IServiceProvider _serviceProvider;
+        public ThemeFileProvider(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            //var pageService = serviceProvider.GetRequiredService<ILongRepository<Page>>();
+            //_context = pageService;
+        }
+
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            return new ThemeDirectoryContent(_serviceProvider, subpath);
+        }
+
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            var result = new ThemeFileInfo(_serviceProvider, subpath);
+            return result.Exists ? result as IFileInfo : new NotFoundFileInfo(subpath);
+        }
+
+        public IChangeToken Watch(string filter)
+        {
+            return new ThemeChangeToken(_serviceProvider, filter);
+        }
+    }
+}
